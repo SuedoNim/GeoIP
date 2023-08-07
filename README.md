640d19cc3c0e1f423fc45d88a0aa8ed6ccf432b6
SuedoNim <donnyhand@gmail.com>
2023-08-08 00:56:52 +0300

    Notes and comments added. Code cleaned up.

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
+# Donny Kon's Note
+This is a standard Angular project written in VSCode.
+Since the solution was made wothout a backend solution, this is purely a Frontend project.
+Running this project should merely involve cloning the code and running it.
+To debug, I recommend starting from app.components.ts, which bears the bulk of the implementation.
+
+# Project Requirements
+The website must have the following features:
+● Show the existing clients.
+    Successful. 
+    By default, an empty search returns the full list.
+● Add and remove a client.
+    Not implemented. 
+    As a beginner angular project for me, I decided the mapping feature took presidence.
+    Due to the learning curve, I ran out of time.
+● Filter the clients (e.g. all the clients with the surname “Cohen”).
+    Successful. 
+    Although no filter required. 
+● Input fields should be validated (at least at minimum level) - both on frontend and backend.
+    Not implemented.
+    Since I didn't need a backend, and I didn't implement an add feature yet.
+
+Extras:
+● For each client IP, show some geo information about the IP address. You can use any API you find
+online. For example - https://ip-api.com/
+    Successful. 
+    Used Open Street Map in the end.
+● Usage of a database. You can use SQL, NoSQL, anything you feel comfortable with.
+    Successful. 
+    Uploaded a mini Sqlite database to DBHub.io. 
+    This allowed me to avoid adding a backend.
+● Responsiveness for different screen sizes (tablets, mobile phones etc).
+    Successful. 
+    Design of the frontend had this in mind.
+
 # GeoIP
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.4.
--- projects/geo-ip/src/app/app.component.ts
+++ projects/geo-ip/src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, ElementRef, Input, NgIterable, Output, TemplateRef, ViewChild, Pipe, PipeTransform } from '@angular/core';
+import { Component, Input, NgIterable } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { NgbModule, NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import { basePlacements } from '@popperjs/core';
-import { Json } from 'sequelize/types/utils';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,28 +8,32 @@ import { Json } from 'sequelize/types/utils';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'GeoIP';
-
+  // Declare Observable Properties
   @Input('users') users : Array<{id:number, name:string, ip:string, phone:string}> = [];
   @Input('selectedUser') selectedUser : {id:number, name:string, ip:string, phone:string} = {id:0,name:'',ip:'',phone:''};
   @Input('coordinates') coordinates : {lon:number, lat:number} = {lon:0,lat:0};
   @Input('mapUrl') mapUrl : {frame:SafeResourceUrl, href: SafeResourceUrl} = {frame:'', href: ''};
   @Input('search') search : string = '';
+
+  // Initialize
+  title = 'GeoIP';
+  closeResult = '';
   domsanitizer: DomSanitizer; 
-	constructor(private offcanvasService: NgbOffcanvas,private sanitizer: DomSanitizer) 
+	constructor(private offcanvasService: NgbOffcanvas, private sanitizer: DomSanitizer) 
   {
     this.domsanitizer = sanitizer;
     this.updateFields(null);
   }
 
-  async getNames(){
-    return this.users.map(item => item.name) as NgIterable<string>;
-  }
-
+  // Use input field to filter users
   async updateFields(event:any) {
-    let search = event == null ? '' : event.target.value;
+    // Get filter. As a level of security, removed the ' character because it can be used to tamper the query. 
+    // Normally stored procedures are recommended, and this is riskier because I didn't add any security to my DB.
+    let search = event == null || event.target.value.includes(`'`) ? '' : event.target.value;
+    // Generate query, default acquires all entires
     let query : string = search == '' ? 'select * from users order by name;': `select * from users where name like '%${search}%' order by name;`;
 
+    // Calling query, and parsing it into the "observable" array - users.
     let body = new FormData();
     body.append('Content-Type', 'application/json');
     body.append('apikey', '2TUCOZeRsi6ul7TgnQTNUrbcVjP');
@@ -64,31 +66,30 @@ export class AppComponent {
 
     console.log(this.users);
   }
-
-  closeResult = '';
   
+  // Open side panel to get details on selected user
 	async open(content:any, index: number) {
+    // Move observable user to the index of the user selected.
     this.selectedUser = this.users[index];
+    // Fetch coordinates
     await fetch(`http://ip-api.com/json/${this.selectedUser.ip}`)
       .then(async (response) => {
         try {
           const data = await response.json();
           this.coordinates = {lon: data.lon, lat: data.lat };
+          // Update links
           let mapUrlFrame = `https://www.openstreetmap.org/export/embed.html?bbox=${this.coordinates.lon}%2C${this.coordinates.lat}%2C${this.coordinates.lon}%2C${this.coordinates.lat}&amp;layer=mapnik&amp;marker=${this.coordinates.lon}%2C${this.coordinates.lat}`;
           let mapUrlHref = `https://www.openstreetmap.org/?mlat=${this.coordinates.lat}&amp;mlon=${this.coordinates.lon}#map=18/${this.coordinates.lat}/${this.coordinates.lon}`;
           this.mapUrl = 
-            {
-              frame: this.domsanitizer.bypassSecurityTrustResourceUrl(mapUrlFrame),
-              href: this.domsanitizer.bypassSecurityTrustResourceUrl(mapUrlHref)
-            };
+          {
+            frame: this.domsanitizer.bypassSecurityTrustResourceUrl(mapUrlFrame),
+            href: this.domsanitizer.bypassSecurityTrustResourceUrl(mapUrlHref)
+          };
         } catch (err) {
           console.log(err);
         }         
       });
-		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
-			(result) => {
-				this.closeResult = `Closed with: ${result}`;
-			}
-		);
+    // Close panel
+    this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' });
   }
 }
