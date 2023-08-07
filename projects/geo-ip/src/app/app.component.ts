a17593bc2bd80dd1e9a12adc529623bcbc149225
SuedoNim <donnyhand@gmail.com>
2023-08-07 20:37:27 +0300

    Done DB Get Integration. Moving on to user display

--- projects/geo-ip/src/app/app.component.ts
+++ projects/geo-ip/src/app/app.component.ts
@@ -1,12 +1,7 @@
-import { Component, ElementRef, TemplateRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, NgIterable, Output, TemplateRef, ViewChild } from '@angular/core';
 import { NgbModule, NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-
-export interface Item {
-  id: number;
-  name: string;
-  ip: string;
-  phone: string;
-}
+import { basePlacements } from '@popperjs/core';
+import { Json } from 'sequelize/types/utils';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +11,53 @@ export interface Item {
 export class AppComponent {
   title = 'GeoIP';
 
-  items : Item[] = [
-  ];
+  @Input('users') users : Array<{id:number, name:string, ip:string, phone:string}> = [];
+  @Input('search') search : string = '';
+
+	constructor(private offcanvasService: NgbOffcanvas) {}
+
+  async getNames(){
+    return this.users.map(item => item.name) as NgIterable<string>;
+  }
+
+  async updateFields(event:any) {
+    let search = event.target.value;
+    let query : string = search == '' ? 'select * from users order by name;': `select * from users where name like '%${search}%' order by name;`;
+
+    let body = new FormData();
+    body.append('Content-Type', 'application/json');
+    body.append('apikey', '2TUCOZeRsi6ul7TgnQTNUrbcVjP');
+    body.append('dbowner', 'SuedoNim');
+    body.append('dbname', 'GeoIP.db');
+    body.append('sql', btoa(query));
+    this.users = [];
+    await fetch('https://api.dbhub.io/v1/query',{ method: 'post', body: body })
+      .then(async (response) => { 
+        try {
+        const data = await response.json();
+        let arr = data
+          .map((item:any) => item
+            .map((item:any) => item['Value']))
+          .filter((item:any)=> item[0] != null);
+        let res:Array<{id:number, name:string, ip:string, phone:string}> = arr.map((item:any)=>{
+          let field: {id:number, name:string, ip:string, phone:string} = {
+            id: item[0],
+            name: item[1],
+            ip: item[2],
+            phone: item[3]
+          }
+          this.users.push(field);
+        });
+          console.log(arr);
+        } catch (err) {
+          console.log(err);
+        } 
+      }) as Array<{id:number, name:string, ip:string, phone:string}>;
+
+    console.log(this.users);
+  }
 
   closeResult = '';
-	constructor(private offcanvasService: NgbOffcanvas) {}
   
 	open(content: any) {
 		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
