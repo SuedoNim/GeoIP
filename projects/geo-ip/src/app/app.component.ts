aedd4e980e8be1a5091d1278597eeffad389d9c3
SuedoNim <donnyhand@gmail.com>
2023-08-08 00:19:25 +0300

    Mapping Works

--- projects/geo-ip/src/app/app.component.ts
+++ projects/geo-ip/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, Input, NgIterable, Output, TemplateRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, NgIterable, Output, TemplateRef, ViewChild, Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { NgbModule, NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { basePlacements } from '@popperjs/core';
 import { Json } from 'sequelize/types/utils';
@@ -12,16 +13,23 @@ export class AppComponent {
   title = 'GeoIP';
 
   @Input('users') users : Array<{id:number, name:string, ip:string, phone:string}> = [];
+  @Input('selectedUser') selectedUser : {id:number, name:string, ip:string, phone:string} = {id:0,name:'',ip:'',phone:''};
+  @Input('coordinates') coordinates : {lon:number, lat:number} = {lon:0,lat:0};
+  @Input('mapUrl') mapUrl : {frame:SafeResourceUrl, href: SafeResourceUrl} = {frame:'', href: ''};
   @Input('search') search : string = '';
-
-	constructor(private offcanvasService: NgbOffcanvas) {}
+  domsanitizer: DomSanitizer; 
+	constructor(private offcanvasService: NgbOffcanvas,private sanitizer: DomSanitizer) 
+  {
+    this.domsanitizer = sanitizer;
+    this.updateFields(null);
+  }
 
   async getNames(){
     return this.users.map(item => item.name) as NgIterable<string>;
   }
 
   async updateFields(event:any) {
-    let search = event.target.value;
+    let search = event == null ? '' : event.target.value;
     let query : string = search == '' ? 'select * from users order by name;': `select * from users where name like '%${search}%' order by name;`;
 
     let body = new FormData();
@@ -59,23 +67,28 @@ export class AppComponent {
 
   closeResult = '';
   
-	open(content: any) {
+	async open(content:any, index: number) {
+    this.selectedUser = this.users[index];
+    await fetch(`http://ip-api.com/json/${this.selectedUser.ip}`)
+      .then(async (response) => {
+        try {
+          const data = await response.json();
+          this.coordinates = {lon: data.lon, lat: data.lat };
+          let mapUrlFrame = `https://www.openstreetmap.org/export/embed.html?bbox=${this.coordinates.lon}%2C${this.coordinates.lat}%2C${this.coordinates.lon}%2C${this.coordinates.lat}&amp;layer=mapnik&amp;marker=${this.coordinates.lon}%2C${this.coordinates.lat}`;
+          let mapUrlHref = `https://www.openstreetmap.org/?mlat=${this.coordinates.lat}&amp;mlon=${this.coordinates.lon}#map=18/${this.coordinates.lat}/${this.coordinates.lon}`;
+          this.mapUrl = 
+            {
+              frame: this.domsanitizer.bypassSecurityTrustResourceUrl(mapUrlFrame),
+              href: this.domsanitizer.bypassSecurityTrustResourceUrl(mapUrlHref)
+            };
+        } catch (err) {
+          console.log(err);
+        }         
+      });
 		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
 			(result) => {
 				this.closeResult = `Closed with: ${result}`;
-			},
-			(reason) => {
-				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-			},
+			}
 		);
   }
-  private getDismissReason(reason: any): string {
-    if (reason === OffcanvasDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === OffcanvasDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on the backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-``}
 }
--- projects/geo-ip/src/app/app.module.ts
+++ projects/geo-ip/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
